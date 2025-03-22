02c2ee35ac8cc19d4997815ac4fdf3db6f0824a2
Harshita <gautamharshita16@gmail.com>
2025-03-23 01:50:01 +0530

    added entity files

---
+++ src/main/java/com/ticket/entity/Department.java
@@ -0,0 +1,25 @@
+package com.ticket.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Department {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	private String name;
+
+}
