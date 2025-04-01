4daf014b3908336be0b9e85d7961cc6c129743ce
Harshita <gautamharshita16@gmail.com>
2025-04-01 23:38:39 +0530

    added security files

---
+++ src/main/java/com/ticket/config/security/UserDetailsServiceImpl.java
@@ -0,0 +1,27 @@
+package com.ticket.config.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.ticket.entity.User;
+import com.ticket.repository.UserRepository;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+	@Autowired
+	private UserRepository userRepo;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userRepo.findByEmail(username);
+		if (user == null) {
+			throw new UsernameNotFoundException("invalid email");
+		}
+		return new CustomUserDetails(user);
+	}
+
+}
